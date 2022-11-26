882e1cbdec6b608d53d7ec4612ac824bf3f91ccf
Trent <trentthebaker@gmail.com>
2022-11-26 00:27:32 -0400

    Radial spawner for asteroids

--- Assets/Asteroids/RadialSpawner.cs
+++ Assets/Asteroids/RadialSpawner.cs
@@ -8,14 +8,14 @@ namespace Asteroids
     {
         [SerializeField] private float minRadius;
         [SerializeField] private float maxRadius;
-        [SerializeField] private float spawnRate;
+        [SerializeField] private float spawnTimer;
         [SerializeField] private GameObject prefab;
 
         private float _timer;
 
         private void Start()
         {
-            _timer = spawnRate;
+            _timer = spawnTimer;
         }
         
         private void Update()
@@ -23,7 +23,7 @@ namespace Asteroids
             _timer -= Time.deltaTime;
             if (_timer <= 0)
             {
-                _timer = spawnRate;
+                _timer = spawnTimer;
                 Spawn();
             }
         }
@@ -32,8 +32,16 @@ namespace Asteroids
         {
             var radius = Random.Range(minRadius, maxRadius);
             var angle = Random.Range(0, 360);
-            var position = new Vector3(radius * Mathf.Cos(angle), radius * Mathf.Sin(angle), 0);
-            Instantiate(prefab, position, Quaternion.identity);
+            var position = new Vector3(radius * Mathf.Cos(angle), 0, radius * Mathf.Sin(angle)) + transform.position;
+            Instantiate(prefab, position, Quaternion.identity, transform);
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawWireSphere(transform.position, minRadius);
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, maxRadius);
         }
     }
 }
