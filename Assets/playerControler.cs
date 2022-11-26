d11e9bcc39a710c01bb0d4b36f4491d7e45a7bb6
Simon-Losier <simon.losier@dal.ca>
2022-11-26 02:24:41 +0000

    rocketship goes forward :steam_locomotive:

---
+++ Assets/playerControler.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControler : MonoBehaviour
+{
+    public Rigidbody rb;
+    public int speed = 200;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(transform.forward * speed * Time.deltaTime);
+    }
+}
