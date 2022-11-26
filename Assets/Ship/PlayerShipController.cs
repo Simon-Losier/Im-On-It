f5e5abdbecc57463e8ec03cdb3a7c646a6b8eee5
Aryah <ar341476@dal.ca>
2022-11-26 00:11:06 -0400

    moved input handling to update() function
    handling input in fixedupdate can cause input loss in the case of single button presses

--- Assets/Ship/PlayerShipController.cs
+++ Assets/Ship/PlayerShipController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Ship
@@ -7,17 +8,32 @@ namespace Ship
         [SerializeField] private Rigidbody rb;
         [SerializeField] private float speed = 125f;
         [SerializeField] private float turnSpeed = 150f;
-    
+        private bool wKey;
+        private bool aKey;
+        private bool dKey;
+
+        private void Update() 
+        {
+            PlayerInput();
+        }
+
+        private void PlayerInput() 
+        {
+            aKey = Input.GetKey(KeyCode.A);
+            wKey = Input.GetKey(KeyCode.W);
+            dKey = Input.GetKey(KeyCode.D);
+        }
+
         private void FixedUpdate()
         {
-            if (Input.GetKey(KeyCode.W))
+            if (wKey)
                 Throttle(speed * Time.deltaTime);
-            if (Input.GetKey(KeyCode.A))
+            if (aKey)
             {
                 transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
             }
 
-            if (Input.GetKey(KeyCode.D))
+            if (dKey)
             {
                 transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
             }
